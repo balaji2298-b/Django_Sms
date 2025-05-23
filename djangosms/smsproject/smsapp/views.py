2f5a98f5bb53e0eecebbadf6dc38383698c8442a
balaji <balajik1924@gmail.com>
2025-05-23 17:43:16 +0530

    Update views.py

--- djangosms/smsproject/smsapp/views.py
+++ djangosms/smsproject/smsapp/views.py
@@ -9,9 +9,9 @@ def sms_form(request):
 		to = request.POST.get('to')
 		message_body = request.POST.get('message')
 
-		account_sid = 'AC25b3adf5a44976b55fb2512723e6adac'
-		auth_token = '1a7bea279473685ce21a8bc71f3feedf'
-		from_number = '+12705136113'
+		account_sid = 'Your_account_sid'
+		auth_token = 'Your_auth_token'
+		from_number = 'Your twilio number'
 
 
 		try:
